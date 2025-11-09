91049cd41957b140848d147ac6b7b281a7e37709
Maksym Zabrodin <m.d.zabrodin@gmail.com>
2025-11-09 22:17:48 +0200

    refactored old tests and added new ones

--- tests/expression_parser_tests.rs
+++ tests/expression_parser_tests.rs
@@ -1,63 +1,76 @@
-use anyhow::{Result, anyhow};
-use logical_expression_parser::parser::parse;
+use anyhow::Result;
+use logical_expression_parser::parser::Rule;
+mod common;
 
 #[test]
-fn test_unexpected_command() -> Result<()> {
-    let input = "(A OR B) And C";
-    let file_pair = parse(input);
-    assert!(file_pair.is_err());
-
-    Ok(())
+fn test_term_valid() -> Result<()> {
+    common::test_if_valid(
+        Rule::term,
+        &["A", "B", "!A", "NOT A", "not A", "!!A", "(A)", "!(A)"],
+    )
 }
 
 #[test]
-fn test_unexpected_identifier() -> Result<()> {
-    let input = "A OR B1";
-    let file_pair = parse(input);
-    assert!(file_pair.is_err());
-
-    Ok(())
+fn test_term_invalid() -> Result<()> {
+    common::test_if_invalid(Rule::term, &["", "1", "a", "*"])
 }
 
 #[test]
-fn test_unclosed_parenthesis() -> Result<()> {
-    let input = "!A OR (B OR !C";
-    let file_pair = parse(input);
-    assert!(file_pair.is_err());
-
-    Ok(())
+fn test_xor_clause_valid() -> Result<()> {
+    common::test_if_valid(
+        Rule::xor_clause,
+        &["A", "A XOR B", "A XOR !B", "A XNOR B", "A XOR !(A XNOR B)"],
+    )
 }
 
 #[test]
-fn test_nested_parentheses() -> Result<()> {
-    let input = "(A OR (B or !C))";
-    let file_pair = parse(input)?
-        .next()
-        .ok_or_else(|| anyhow!("No file pair"))?;
-    let expression_pair = file_pair
-        .into_inner()
-        .next()
-        .ok_or_else(|| anyhow!("No expression"))?;
-    assert_eq!(expression_pair.as_str(), input);
-
-    Ok(())
+fn test_xor_clause_invalid() -> Result<()> {
+    common::test_if_invalid(
+        Rule::xor_clause,
+        &["XOR B C", "XOR !B", "XOR !(A XNOR B)", ""],
+    )
 }
 
 #[test]
-fn test_simple_or() -> Result<()> {
-    let input = "A OR B";
-    let file_pair = parse(input)?
-        .next()
-        .ok_or_else(|| anyhow!("No file pair"))?;
-    let expression_pair = file_pair
-        .into_inner()
-        .next()
-        .ok_or_else(|| anyhow!("No expression"))?;
-    assert_eq!(
-        expression_pair.as_rule(),
-        logical_expression_parser::parser::Rule::expression
-    );
-    assert_eq!(expression_pair.as_str(), input);
+fn test_and_clause_valid() -> Result<()> {
+    common::test_if_valid(
+        Rule::and_clause,
+        &[
+            "A",
+            "A AND B",
+            "A NAND B",
+            "A AND B AND C",
+            "A AND !B",
+            "A XOR B AND C",
+        ],
+    )
+}
 
-    Ok(())
+#[test]
+fn test_and_clause_invalid() -> Result<()> {
+    common::test_if_invalid(
+        Rule::and_clause,
+        &["AND A B", "AND B", "AND C", "AND !B", ""],
+    )
+}
+
+#[test]
+fn test_expression_valid() -> Result<()> {
+    common::test_if_valid(
+        Rule::expression,
+        &[
+            "A",
+            "B",
+            "!A",
+            "A OR B",
+            "A OR !!!(B XOR H AND F)",
+            "A NOR B AND C",
+            "A XOR F AND !B",
+        ],
+    )
+}
+
+#[test]
+fn test_expression_invalid() -> Result<()> {
+    common::test_if_invalid(Rule::expression, &["OR A B", "OR", "NOR", "NOR A B", ""])
 }
