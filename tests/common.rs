867c4da674d24fcfec4e7c0a65e406a0432a2e17
Maksym Zabrodin <m.d.zabrodin@gmail.com>
2025-11-09 22:05:52 +0200

    common methods used to test grammar rules

---
+++ tests/common.rs
@@ -0,0 +1,21 @@
+use logical_expression_parser::parser::{Grammar, Rule};
+use pest::Parser;
+
+pub fn test_if_valid(rule: Rule, data: &[&str]) -> anyhow::Result<()> {
+    for some in data {
+        let pair = Grammar::parse(rule, some)?.next().unwrap();
+        assert_eq!(pair.as_rule(), rule);
+        assert_eq!(pair.as_str(), some.to_string());
+    }
+
+    Ok(())
+}
+
+pub fn test_if_invalid(rule: Rule, data: &[&str]) -> anyhow::Result<()> {
+    for some in data {
+        let pair = Grammar::parse(rule, some);
+        assert!(pair.is_err());
+    }
+
+    Ok(())
+}
