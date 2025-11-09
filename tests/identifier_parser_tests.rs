9a9ddd9fd84e1395f21804d207160ab3c623cf46
Maksym Zabrodin <m.d.zabrodin@gmail.com>
2025-11-09 20:06:56 +0200

    add unit tests for identifier rule from grammar

---
+++ tests/identifier_parser_tests.rs
@@ -0,0 +1,27 @@
+use anyhow::Result;
+use logical_expression_parser::parser::{Grammar, Rule};
+use pest::Parser;
+
+#[test]
+fn test_identifier_valid() -> Result<()> {
+    let pairs = Grammar::parse(Rule::identifier, "A")?;
+    assert_eq!(pairs.as_str(), "A");
+
+    Ok(())
+}
+
+#[test]
+fn test_identifier_invalid_lowercase() -> Result<()> {
+    let pairs = Grammar::parse(Rule::identifier, "a");
+    assert!(pairs.is_err());
+
+    Ok(())
+}
+
+#[test]
+fn test_identifier_only_single_character() -> Result<()> {
+    let pairs = Grammar::parse(Rule::identifier, "VAR")?;
+    assert!(pairs.as_str().starts_with("V"));
+
+    Ok(())
+}
