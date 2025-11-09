45290dcbcd4230bb04f8fc1238b4f4a40059299f
Maksym Zabrodin <m.d.zabrodin@gmail.com>
2025-11-09 22:06:18 +0200

    refactored using common.rs

--- tests/operators_parser_tests.rs
+++ tests/operators_parser_tests.rs
@@ -1,93 +1,73 @@
 use anyhow::Result;
-use logical_expression_parser::parser::{Grammar, Rule};
-use pest::Parser;
-
-fn test_some_operator_forms_valid(some_operator: Rule, forms: &[&str]) -> Result<()> {
-    for operator in forms {
-        let pair = Grammar::parse(some_operator, operator)?.next().unwrap();
-        assert_eq!(pair.as_rule(), some_operator);
-        assert_eq!(pair.as_str(), operator.to_string());
-        assert!(pair.clone().into_inner().next().is_none());
-    }
-
-    Ok(())
-}
-
-fn test_some_operator_forms_invalid(some_operator: Rule, forms: &[&str]) -> Result<()> {
-    for operator in forms {
-        let result = Grammar::parse(some_operator, operator);
-        assert!(result.is_err());
-    }
-
-    Ok(())
-}
+use logical_expression_parser::parser::Rule;
+mod common;
 
 #[test]
 fn test_not_operator_forms_valid() -> Result<()> {
-    test_some_operator_forms_valid(Rule::not_operator, &["NOT", "not", "!"])
+    common::test_if_valid(Rule::not_operator, &["NOT", "not", "!"])
 }
 
 #[test]
 fn test_and_operator_forms_valid() -> Result<()> {
-    test_some_operator_forms_valid(Rule::and_operator, &["AND", "and", "&"])
+    common::test_if_valid(Rule::and_operator, &["AND", "and", "&"])
 }
 
 #[test]
 fn test_nand_operator_forms_valid() -> Result<()> {
-    test_some_operator_forms_valid(Rule::nand_operator, &["NAND", "nand", "!&"])
+    common::test_if_valid(Rule::nand_operator, &["NAND", "nand", "!&"])
 }
 
 #[test]
 fn test_or_operator_forms_valid() -> Result<()> {
-    test_some_operator_forms_valid(Rule::or_operator, &["OR", "or", "|"])
+    common::test_if_valid(Rule::or_operator, &["OR", "or", "|"])
 }
 
 #[test]
 fn test_nor_operator_forms_valid() -> Result<()> {
-    test_some_operator_forms_valid(Rule::nor_operator, &["NOR", "nor", "!|"])
+    common::test_if_valid(Rule::nor_operator, &["NOR", "nor", "!|"])
 }
 
 #[test]
 fn test_xor_operator_forms_valid() -> Result<()> {
-    test_some_operator_forms_valid(Rule::xor_operator, &["XOR", "xor", "^"])
+    common::test_if_valid(Rule::xor_operator, &["XOR", "xor", "^"])
 }
 
 #[test]
 fn test_xnor_operator_forms_valid() -> Result<()> {
-    test_some_operator_forms_valid(Rule::xnor_operator, &["XNOR", "xnor", "!^"])
+    common::test_if_valid(Rule::xnor_operator, &["XNOR", "xnor", "!^"])
 }
 
 #[test]
 fn test_not_operator_forms_invalid() -> Result<()> {
-    test_some_operator_forms_invalid(Rule::not_operator, &["Not", "nOt"])
+    common::test_if_invalid(Rule::not_operator, &["Not", "nOt"])
 }
 
 #[test]
 fn test_and_operator_forms_invalid() -> Result<()> {
-    test_some_operator_forms_invalid(Rule::and_operator, &["And", "aNd", "||"])
+    common::test_if_invalid(Rule::and_operator, &["And", "aNd", "||"])
 }
 
 #[test]
 fn test_nand_operator_forms_invalid() -> Result<()> {
-    test_some_operator_forms_invalid(Rule::nand_operator, &["Nand", "nAnd", "!|"])
+    common::test_if_invalid(Rule::nand_operator, &["Nand", "nAnd", "!|"])
 }
 
 #[test]
 fn test_or_operator_forms_invalid() -> Result<()> {
-    test_some_operator_forms_invalid(Rule::or_operator, &["Or", "o r", "&&"])
+    common::test_if_invalid(Rule::or_operator, &["Or", "o r", "&&"])
 }
 
 #[test]
 fn test_nor_operator_forms_invalid() -> Result<()> {
-    test_some_operator_forms_invalid(Rule::nor_operator, &["Nor", "n O r", "!&"])
+    common::test_if_invalid(Rule::nor_operator, &["Nor", "n O r", "!&"])
 }
 
 #[test]
 fn test_xor_operator_forms_invalid() -> Result<()> {
-    test_some_operator_forms_invalid(Rule::xor_operator, &["Xor", "xOr", "!"])
+    common::test_if_invalid(Rule::xor_operator, &["Xor", "xOr", "!"])
 }
 
 #[test]
 fn test_xnor_operator_forms_invalid() -> Result<()> {
-    test_some_operator_forms_invalid(Rule::xnor_operator, &["Xnor", "xNoR", "%"])
+    common::test_if_invalid(Rule::xnor_operator, &["Xnor", "xNoR", "%"])
 }
